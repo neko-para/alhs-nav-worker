679afa8423d777b5150b9f5a0f05204b7c19a76d
nekosu <liao.hy@outlook.com>
2021-06-11 12:43:39 +0800

    first commit

---
+++ core.js
@@ -0,0 +1,106 @@
+function IsPC() {  
+	var userAgentInfo = navigator.userAgent;
+	var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+	var flag = true;
+	Agents.forEach(function (a) {
+		if (userAgentInfo.indexOf(a) > 0) {
+			flag = false;
+		}
+	});
+	return flag;
+}
+var img = new Image();
+img.onload = function () {
+	var atan = img.height / img.width;
+	var mx = 0.5, my = 0.5;
+	
+	function update() {
+		var ratan = window.innerHeight / window.innerWidth;
+		let height, width;
+		let ofsx, ofsy;
+		if (atan >= ratan) {
+			width = window.innerWidth;
+			height = width * atan;
+			ofsx = 0;
+			ofsy = -(height - window.innerHeight) * my;
+		} else {
+			height = window.innerHeight;
+			width = height / atan;
+			ofsx = -(width - window.innerWidth) * mx;
+			ofsy = 0;
+		}
+		img.setAttribute('style', 'top: ' + ofsy + 'px; left: ' + ofsx + 'px; width: ' + width + 'px; height: ' + height + 'px;')
+	};
+	window.onresize = update;
+	document.body.onmousemove = function (e) {
+		mx = e.clientX / window.innerWidth;
+		my = e.clientY / window.innerHeight;
+		update();
+	};
+	update();
+	$('body').prepend(img);
+};
+if (IsPC()) {
+	img.src = `${ROOT}/BG.jpg`;
+} else {
+	img.src = `${ROOT}/BG_mb.jpg`;
+	document.body.setAttribute('class', 'mobile');
+}
+$(document).ready(function () {
+	$('#black').click(function () {
+		$('#black').attr('class', 'hide');
+	});
+	var info = JSON.parse(INFO);
+	$('.title').text(info.title);
+	[ 'dom', 'int', 'cdn'].forEach(t => {
+		var desc = {
+			normal: '正常',
+			unstable: '不稳定',
+			maintain: '维护中'
+		}[info.state[t]];
+		var item = $(`#state > .state_item[alt="${t}"]`);
+		$('.state_circ', item).attr('alt', info.state[t]);
+		$('.state_text', item).text(desc);
+	});
+	info.subt.forEach(function (str) {
+		$('#inner').append($('<span />').text(str));
+	});
+	info.link.forEach(function (lk) {
+		var lnk = $('<a target="_blank" />').attr('href', lk.target);
+		if (IsPC()) {
+			if (lk.name.indexOf('\n') === -1) {
+				lnk.text(lk.name);
+			} else {
+				var sp = lk.name.split('\n');
+				lnk.text(sp[0]);
+				lnk.hover(function () {
+					lnk.text(sp[1]);
+				}, function () {
+					lnk.text(sp[0]);
+				})
+			}
+		} else {
+			lnk.text(lk.name.replace('\n', ' '));
+		}
+		$('#link').append(lnk);
+	});
+	info.announce.forEach(function (obj) {
+		var dt = obj.content.split('\n');
+		var sp = $('<span />').text(obj.title).click(function () {
+			$('#detail p').remove();
+			dt.forEach(s => {
+				$('#detail').append($('<p />').text(s));
+			});
+			$('#black').attr('class', 'show');
+		});
+		if (obj.important) {
+			sp.attr('class', 'important');
+		}
+		$('#announce').append(sp);
+	});
+	$('#inner_wrap').attr('class', 'show');
+	document.getElementById('inner_wrap').addEventListener('transitionend', function () {
+		$('#padding').css('height', $('#board').height() + 50);
+		$('#board').css('top', $('#content').position().top + $('#content').height() + 50).css('opacity', 1);
+	}, false);
+});
---
+++ index.js
@@ -0,0 +1,166 @@
+
+addEventListener("fetch", event => {
+	event.respondWith(handleRequest(event.request))
+})
+
+root = "https://cdn.jsdelivr.net/gh/neko-para/alhs-nav-worker@1.0.0"
+
+info = JSON.parse(`{
+    "announce": [
+        {
+            "content": "文豪试炼四期–周年站庆大赛：\n\n参赛要求：\n①字数3000+\n②必须本人原创\n③参赛文章需要至少要有以下三个标签中的一个（即文中要出现相关情节或物品）\n#SΜ   #扶她    #工口饰品\n④禁止赌毒政内容!\n\n征稿相关：\n\n本活动征稿均使用cc-BY-NC-SA4.0协议\n单作者仅限一篇作品参赛\n\n#0投稿时间：6月15日-6月30日\n比赛时间：7月1日-7月15日\n（在#0投稿时间投稿的参赛文章会在比赛第一天统一发布。推荐）\n\n本活动最终解释权归图书馆所有\n\n优胜奖励：\n奖金250￥+图书馆状元/榜眼/探花认证+文章页/首页推荐\n\n详情请至活动页\nalhs.fun/index.php/huodong/",
+            "title": "文豪试炼四期–周年站庆大赛",
+            "important": true
+        },
+        {
+            "content": "艾利浩斯学院 图书馆 于 2021年5月22日22:00(UTC+8)更新域名为 https://alhs.fun ,公告发布页为 http://alhs.live .\n原域名访问会跳转至新域名（此功能需科学上网）\n原公告发布页依然可以正常访问。\n感谢各位对图书馆的支持",
+            "title": "[5.22]图书馆域名正式更新公告",
+            "important": true
+        },
+        {
+            "content": "各位图书馆的用户，日安\n因为图书馆原有域名遭到GFW屏蔽，我们计划近日迁移至新域名，图书馆数据也会一并迁移。\n很抱歉对各位造成了麻烦，也感谢在图书馆无法访问时对我们表达关心的各位。\n 原域名 ailihaosi.xyz     新域名 alhs.fun\n（域名更换工作需要数日进行）\n亲爱的各位源泉，我们将于以下时间，对图书馆进行临时维护。\n维护期间，图书馆可能无法访问\n[维护时间]5月15日20:30-18日2:00（预计）\n[维护内容]图书馆域名更新\n\n[补偿内容]图书馆积分x1\n[补偿对象]5月15日12:00前创建账号的用户\n[补偿方式]发送至对应账户\n[发送时间]5月20日1:00",
+            "title": "[5.15]图书馆域名更新&不停机维护"
+        },
+        {
+            "content": "亲爱的各位源泉，图书馆网络近期出现了部分地区无法访问的情况，如遇此类问题请多次刷新即可正常访问，技术人员正在修复中",
+            "title": "[5.9]图书馆网络波动公告"
+        },
+        {
+            "content": "非常抱歉，由于若雨本人疏忽导致CDN忘记续费，导致图书馆出现了约48小时的无法访问的情况，若雨在此向各位道歉（鞠躬），还请各位多多包涵。图书馆已恢复正常访问。",
+            "title": "[4.23]图书馆访问异常公告"
+        },
+        {
+            "content": "发布页新增图书馆服务器状态展示，其中“全球”指除大陆以外的地区，“CDN”指缓存服务器，若缓存服务器异常则会定向至源站\n\n近期由于大陆会议，图书馆访问不稳定属正常情况",
+            "title": "[3.7]图书馆发布页更新&图书馆网络波动公告"
+        },
+        {
+            "content": "亲爱的各位源泉，我们将于以下时间，对图书馆进行不停机维护。\n维护期间，图书馆部分功能可能会出现异常或无法访问。\n\n[维护时间]2021年2月26日20:15-21:00（预计）\n[维护内容]CDN服务器热更新\n[补偿内容]图书馆积分x1\n[补偿对象]2月26日12:00前创建账号的用户\n[补偿方式]发送至对应账户\n[发送时间]2月26日1:00\n\n艾利浩斯学院图书馆祝各位元宵节快乐",
+            "title": "[2.26]图书馆不停机维护通知"
+        },
+        {
+            "content": "亲爱的各位源泉，我们将于以下时间，对图书馆进行不停机维护。\n维护期间，图书馆部分功能可能会出现异常或无法访问。\n\n[维护时间]2021年2月15日21:30-次日1:30（预计）\n[维护内容]图书馆CDN更换\n[补偿内容]图书馆积分x0\n[补偿对象]2月15日12:00前创建账号的用户\n\nCDN全面更换完毕后，大陆地区访问图书馆的速度会极大提升",
+            "title": "[2.15]图书馆不停机维护通知"
+        },
+        {
+            "content": "因为开发语言等多种原因，图书馆APP2.0.0将于2021年2月1日正式停止维护，各位依然可以继续使用本APP，但是本APP的bug报告将不再受理，也不再会有后续更新。\n各位也可以使用浏览器访问ailihaosi.xyz来访问图书馆。\n图书馆将另开发新版图书馆APP。",
+            "title": "[1.31]图书馆APP停止维护公告"
+        },
+        {
+            "content": "Investigating\nSG VPS Clients may experience service interruptions during this time, downtime for up to 15 minutes is expected. SG NME switch replacement VPS clients affected. Thank You for Your patience.\nPosted . Jan 08, 2021 - 13:03 UTC ",
+            "title": "[1.8]图书馆网络波动通知"
+        },
+        {
+            "content": "亲爱的各位源泉，我们将于以下时间，对图书馆进行不停机维护。\n维护期间，图书馆部分功能可能会出现异常或无法访问。\n\n[维护时间]2021年1月5日（预计）\n[维护内容]图书馆访客系统恢复\n[补偿内容]图书馆积分x1\n[补偿对象]1月1日12:00前创建账号的用户\n[补偿方式]发送至对应账户\n[发送时间]1月6日1:00\n\n除旧岁，迎新春   艾利浩斯学院图书馆祝各位新年快乐",
+            "title": "[1.1]图书馆不停机维护通知"
+        },
+        {
+            "content": "艾利浩斯图书馆公告\n图书馆积极响应号召，即日起不再允许游客访问图书馆文章内容，并下线所有二维码。图书馆内部也将自我反思，以上整改内容恢复日期待定，感谢各位对图书馆的支持。\n若雨  2020年12月24日",
+            "title": "[12.24]图书馆公告"
+        },
+        {
+            "content": "Cloudflare 中国区域出现解析故障\n\n20:30 左右，在中国内地访问DNS托管于Cloudflare的网站开始出现解析失败，提示“此网站无法提供安全连接”。\n\n包括 Cloudflare 官网在内的网站无法通过中国内地网络直接访问。大部分域名被指向到了位于中国内地的IP上，所有的解析均没有成功。\n\n目前，还不清楚这是否为单一的技术故障。CF未就此发布状态异常警告。",
+            "title": "[12.15]图书馆网络波动通知"
+        },
+        {
+            "content": "亲爱的各位源泉，我们将于以下时间，对图书馆进行临时维护。维护期间，您暂时无法访问图书馆。\n[维护时间]12月13日23:10-12月13日23:11（预计）\n[维护内容]图书馆基石内容更新 \n[补偿内容]图书馆积分x1\n[补偿对象]12月13日12:00前创建账号的用户\n[补偿方式]发送至对应账户\n[发送时间]12月14日1:00",
+            "title": "[12.13]图书馆停机维护通知"
+        },
+        {
+            "content": "亲爱的各位源泉我们正在进行服务器停机维护，图书馆将无法访问。\n图书馆新加坡服务器受到了大流量的DDOS攻击，现场技术人员已经在努力解决问题",
+            "title": "[11.17]图书馆停机维护通知"
+        },
+        {
+            "content": "亲爱的各位源泉，我们将于以下时间，对图书馆进行临时维护。维护期间，您暂时无法访问图书馆。\n[维护时间] 10月17日21:00-22:00（预计）\n[维护内容] 我们从ISP获得了更多信息，他们遇到了大规模的网络中断，并且现场技术人员已经在努力解决此问题。",
+            "title": "[10.17]图书馆紧急停机维护通知"
+        },
+        {
+            "content": "亲爱的各位源泉， 我们将于以下时间，对图书馆进行临时维护。维护期间，您暂时无法访问图书馆。\n[维护时间] 10月11日10:00-12:00（预计） \n[维护内容] 图书馆UI样式升级\n[补偿内容] 活动硬币x1\n[补偿对象] 10月11日12:00前创建账号的用户",
+            "title": "[10.11]图书馆停机维护通知"
+        },
+        {
+            "content": "图书馆官方将对2020年10月前注册的账号进行排查，其中注册邮箱胡乱编造者封停账号永久，已使用违规账号的用户请及时转移数据。",
+            "title": "[10.1]关于开展注册邮箱异常账号的清理的说明"
+        }
+    ],
+    "link": [
+        {
+            "name": "图书馆主站\nalhs.fun",
+            "target": "https://alhs.fun/"
+        },
+        {
+            "name": "图书馆备用\nailihaosi.xyz <需科学上网>",
+            "target": "https://ailihaosi.xyz/"
+        },
+        {
+            "name": "telegram频道",
+            "target": "https://t.me/ailihaosi/"
+        },
+        {
+            "name": "tg频道直连",
+            "target": "https://post.ailihaosi.xyz/"
+        }
+    ],
+    "state": {
+        "cdn": "normal",
+        "dom": "normal",
+        "int": "normal"
+    },
+    "subt": [
+        "建议收藏本页以免丢失位点",
+        "alhs.live"
+    ],
+    "title": "艾利浩斯图书馆传送位点站"
+}
+`)
+
+async function handleRequest(request) {
+	const init = {
+	  headers: {
+		'content-type': 'text/html;charset=UTF-8',
+	  },
+	}
+	return new Response(
+`<html lang="zh">
+	<head>
+		<title>艾利浩斯学院图书馆导航页</title>
+		<meta charset="UTF-8">
+		<link rel="icon" href="${root}/32.png">
+		<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+		<link href="${root}/style.css" rel="stylesheet">
+	</head>
+	
+	<body>
+        <script>var INFO=JSON.parse(\`${JSON.stringify(info)}\`);var ROOT="${root}"</script>
+		<div id="wrap">
+			<div id="content"><img src="${root}/192.png">
+				<div id="inner_wrap" class="hide">
+					<div id="inner"><span class="title"></span></div>
+					<p>图书馆服务器支持：</p>
+					<div id="state">
+						<div alt="dom" class="state_item">大陆：<span class="state_text">加载中</span>
+							<div class="state_circ"></div>
+						</div>
+						<div alt="int" class="state_item">全球：<span class="state_text">加载中</span>
+							<div class="state_circ"></div>
+						</div>
+						<div alt="cdn" class="state_item">CDN：<span class="state_text">加载中</span>
+							<div class="state_circ"></div>
+						</div>
+					</div>
+				</div><span id="abvlnk">传送门</span>
+				<div id="link"></div>
+				<div id="board"><span>公告</span>
+					<div id="announce"></div>
+				</div>
+			</div>
+		</div>
+		<div id="black" class="hide">
+			<div id="detail" class="hide"></div>
+		</div>
+		<div id="padding"></div>
+		<div id="footer"><span>&copy;2020-2021 艾利浩斯图书馆</span></div>
+		<script src="${root}/core.js"></script>
+	</body>
+	
+	</html>`, init);
+}
